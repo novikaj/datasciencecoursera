e9bd55ed95efd36588019c9d460b079d8ba301d1
novikaj <novikaj@ukma.edu.ua>
2020-05-07 17:32:48 +0300

    Update plus matrix

--- course2trial.r
+++ course2trial.r
@@ -1,6 +1,5 @@
 getwd()
 
-)
 y=6
 class(x)
 as.numeric(x)
@@ -8,3 +7,16 @@ as.logical(x)
 as.character(x)
 x=0:6
 as.logical(x)
+b=list(c,"a",TRUE)
+m=matrix(nrow=2, ncol=3)
+dim(m)
+attributes(m)
+
+m=matrix(1:6, nrow=2,ncol=3)
+n=1:10
+dim(n)=c(2,5)
+n
+x=1:3
+y=10:12
+cbind(x,y)
+rbind(x,y)
