362927f272294fdaa77c9cb88d6419ec8096ad84
novikaj <novikaj@ukma.edu.ua>
2020-05-29 22:20:36 +0300

    subsetting double brackets

--- course2removebags.R
+++ course2removebags.R
@@ -1,6 +1,20 @@
+<<<<<<< HEAD
 x<-list(foo=1:4,bar=0.6)
 x[1]
 x[[1]]
 x$bar
 x[["bar"]]
 x["bar"]
+=======
+x<-list(foo=1:4,bar=0.6)
+x[1]
+x[[1]]
+x$bar
+x[["bar"]]
+x["bar"]
+x<-list(a=list(10,12,14), b=c(3.14,2.81))
+x[[c(1,3)]]
+x[[1]][[3]]
+x[[c(2,1)]]
+
+>>>>>>> 86b7737... subsetting double brackets
