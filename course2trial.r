651348db56e3b03b95c976a6260aec1891e5b87b
novikaj <novikaj@ukma.edu.ua>
2020-05-29 20:27:24 +0300

    dump(y,x)

--- course2trial.r
+++ course2trial.r
@@ -53,13 +53,10 @@ m
 dimnames(m)=list(c("a","b"),c("c","d"))
 m
 print("Hello World")
-y<-data.frame(a=1,b="a")
-dput(y)
-y=data(a=1, b="a")
+x="foo"
 y=data.frame(a=1, b="a")
-dput(y)
-dput(y, file="y.R")
-new.y=dget("y.R")
-new.y
-
-
+dump(c("x","y"),file="data.R")
+rm(x,y)
+source("data.R")
+y
+x
