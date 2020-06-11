eaed9eafd0a19e5cdb0aee21544d80ba675c9c9d
novikaj <novikaj@ukma.edu.ua>
2020-06-12 01:41:17 +0300

    My first Function

---
+++ function.r
@@ -0,0 +1,23 @@
+add2<-function(x,y) {
+  x+y
+}
+
+above10<-function(x) {
+  use<-x>10
+  x[use]
+}
+
+above<-function(x,n){
+  use<-x>n
+  x[use]
+}
+
+columnmean<-function(y,removeNA=TRUE) {
+  nc<-ncol(y)
+  means<-numeric(nc)
+  for (i in 1:nc) {
+    means[i]<-mean(y[,1],na.rm = removeNA)
+  }
+  means
+  
+}
