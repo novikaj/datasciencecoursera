445a63d18058fb0e03eb3972bed6a372ec75b6b4
novikaj <novikaj@ukma.edu.ua>
2020-06-12 15:12:06 +0300

    Functions

--- function.r
+++ function.r
@@ -21,3 +21,37 @@ columnmean<-function(y,removeNA=TRUE) {
   means
   
 }
+
+mydata<-rnorm(100)
+sd(mydata)
+sd(x=mydata)
+sd(x=mydata,na.rm=FALSE)
+sd(na.rm=FALSE, x=mydata)
+sd(na.rm=FALSE, mydata)
+x
+sd(x)
+sd(x=mydata)
+
+args(lm)
+
+f<-function(a,b=1,c=2,d=NULL) {
+  a^2
+}
+
+f(2)
+
+f<-function(a,b) {
+  print(a)
+  print(b)
+}
+
+f(45)
+  
+myplot<-function(x,y,type="1",...) {
+  plot(x,y,type=type,...)
+}
+
+args(paste)
+args(cat)
+paste("a","b",sep=":")
+paste("a","b",se=":")
